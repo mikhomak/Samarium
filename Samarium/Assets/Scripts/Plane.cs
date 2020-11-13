4d991f4f3aaf58f80f38dd5e903952826116b298
Misha <homakhaker@gmail.com>
2020-11-13 20:32:58 +0100

    LET'S FUCKING GOOOOOOO

---
+++ Samarium/Assets/Scripts/Plane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour {
+
+    [SerializeField] private Rigidbody rbd;
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    private void FixedUpdate() {
+    }
+}
---
+++ Samarium/Assets/Scripts/Stats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Stats : MonoBehaviour {
+    // CONSTS
+    public const float ACCELERATION_THROTTLE_DOWN = 200f;
+    public const float ACCELERATION_NO_THROTTLE = 200f;
+    public const float ACCELERATION_THROTTLE_UP = 200f;
+    public const float AIR_CONTROL = 200f;
+    public const float AERODYNAMIC = 200f;
+    public const float LERP_VALUE_VELOCITY = 200f;
+    
+    
+    public float pitchControl = 200f;
+    public float yawnControl = 200f;
+    public float rollControl = 200f;
+    
+    public float maxSpeed = 400f;
+}
